8742f323ad63dcdc7cca15b7599b590413403fec
Kavana Inamdar <kavanainamdar@outlook.com>
2021-07-21 16:44:54 -0700

    Datatypes covered...

---
+++ java/src/Datatypes.java
@@ -0,0 +1,22 @@
+public class Datatypes {
+    public static void main (String[] args) {
+        int a = 0;
+        char c = 'c';
+        boolean isSunnyOut = true;
+        String name = "hi there";
+        Double sample = 10.9;
+
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(isSunnyOut);
+        System.out.println(name);
+        System.out.println(sample);
+
+        System.out.println("a");
+        System.out.println("c");
+        System.out.println("isSunnyOut");
+        System.out.println("name");
+        System.out.println("sample");
+
+    }
+}
