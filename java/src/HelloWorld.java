885a5a144c93385e8246cbe6328513c82d20a0dc
Kavana Inamdar <kavanainamdar@outlook.com>
2021-07-20 18:32:32 -0700

    My first java project !! Hurray :-)

---
+++ java/src/HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld {
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+		
+		
+		
+		System.out.println("Hello World");
+		System.out.println("Hello Kavana");
+		
+		System.out.println("Hello Mandar");
+		System.out.println("Hello baba, " + " Mandar. You are my baba, " + "and Varsha is my mamma.");
+		System.out.print("Hello mamma, ");
+		System.out.print("You are awesome. ");
+		System.out.print("Love, Kavana.");
+		trythis();
+		
+
+		
+	}
+	
+	public static void trythis () {
+		
+		System.out.println("Hi baba");
+	}
+	
+}
+
+
+
+
+
