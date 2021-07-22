a1ba48f76cf460fdb6d17c4b592f8e75b20b5a14
Kavana Inamdar <kavanainamdar@outlook.com>
2021-07-21 17:30:44 -0700

    Adds Variable Manipulation java file, restucture folder structure

---
+++ src/VarManipulation.java
@@ -0,0 +1,11 @@
+public class VarManipulation {
+    public static void main(String[] args) {
+        int age = 25;
+        double add = .9;
+
+        System.out.println(age + add);
+        System.out.println(age * add);
+
+    }
+    
+}
